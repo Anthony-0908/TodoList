bec454c2b5471ad889b9bc8a51d54e848b2a39b9
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-07-27 00:49:04 +0800

    todo list

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,25 +1,58 @@
-import { Component } from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms'
+import { Component,signal } from '@angular/core';
+import {FormBuilder, FormGroup, Validators,} from '@angular/forms'
+
+interface Todo {
+  id:number,
+  title:string,
+  completed:boolean
+}
+
 @Component({
   selector: 'app-todo-list',
-  imports: [ReactiveFormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-   name = new FormControl('');
-  // UpdateField(): void {
-  //   console.log("this is new field")
-  // }
+todos:Todo[] = [];
+form!: FormGroup;
 
-  // count = 0;
+   constructor(private fb: FormBuilder) {
+    this.form = this.fb.nonNullable.group({
+      title: ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
 
-  // inc(){this.count++;
-  //   console.log('Increment value',this.count);
-  // }
 
-  // dec(){this.count--;
-  //   console.log('Decrement value' ,this.count);
-  // }
+  addTodo() {
+    if(this.form.invalid) return;
+
+    const {title} = this.form.getRawValue();
+    this.todos.push({id: Date.now(), title, completed:false});
+    this.form.reset();
+
+  }
+
+
+  toggle(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter(t => t.id !== id);
+  }
+  //  name = new FormControl('');
+  // // UpdateField(): void {
+  // //   console.log("this is new field")
+  // // }
+
+  // // count = 0;
+
+  // // inc(){this.count++;
+  // //   console.log('Increment value',this.count);
+  // // }
+
+  // // dec(){this.count--;
+  // //   console.log('Decrement value' ,this.count);
+  // // }
 
 }
--- src/app/features/todo/todo.component.ts
+++ src/app/features/todo/todo.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
-import { TodoListComponent } from '../../components/todo-list/todo-list.component';
-@Component({
+import { Component,signal } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
+
+interface Todo {
+  id:number,
+  title:string,
+  completed:boolean
+}@Component({
   selector: 'app-todo',
-  imports: [TodoListComponent],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
- task = "Learn angular basics"
+todos:Todo[] = [];
+form!: FormGroup;
+
+   constructor(private fb: FormBuilder) {
+    this.form = this.fb.nonNullable.group({
+      title: ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
+
+
+  addTodo() {
+    if(this.form.invalid) return;
+
+    const {title} = this.form.getRawValue();
+    this.todos.push({id: Date.now(), title, completed:false});
+    this.form.reset();
+
+  }
+
+
+  toggle(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter(t => t.id !== id);
+  }
 }
